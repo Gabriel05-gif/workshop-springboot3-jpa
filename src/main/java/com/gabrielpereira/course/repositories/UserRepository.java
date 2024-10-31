100995515457c0e10bd683f0e405d12494327f9c
Gabriel Pereira de Sousa <gabrielpereira.gn@gmail.com>
2024-10-31 12:14:26 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/gabrielpereira/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.gabrielpereira.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gabrielpereira.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+}
