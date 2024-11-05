c323b12ba680a2f7a774c1bd7236c2067e34d0e7
Gabriel Pereira de Sousa <gabrielpereira.gn@gmail.com>
2024-11-05 18:32:44 -0300

    OrderItem, many-to-many association with extra attributes PART - 2

---
+++ src/main/java/com/gabrielpereira/course/repositories/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package com.gabrielpereira.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gabrielpereira.course.entities.OrderItem;
+
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+	
+}
