6f35aa2f0ee6ee5a614f55244f091812b3b68b71
Gabriel Pereira de Sousa <gabrielpereira.gn@gmail.com>
2024-11-06 17:04:22 -0300

    Exception handling - update

--- src/main/java/com/gabrielpereira/course/services/UserService.java
+++ src/main/java/com/gabrielpereira/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.gabrielpereira.course.repositories.UserRepository;
 import com.gabrielpereira.course.services.exceptions.DatabaseException;
 import com.gabrielpereira.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -44,9 +46,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
